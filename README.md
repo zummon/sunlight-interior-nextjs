0c921b1fdaadc1050d2aab87418cfba62c3d5778
zummon <zummon.space@gmail.com>
2023-09-16 10:50:03 +0700

    prepare for tailwind css

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-### [Codesandbox](https://codesandbox.io/p/github/zummon/sunlight-interior-nextjs)
+Modify your data in `./lib/services.json` to add, edit and remove the site pages.
 
-- Modify your data in `./lib/services.json` to add, edit and remove the site pages.
+https://www.svgrepo.com/svg/317403/decoration
 
 Something breaks, needs upgrade. Let me know.
-
-### Dev tools
-
-- [UIkit](https://getuikit.com/)
-- [Photo by Rachel Claire from Pexels](https://www.pexels.com/photo/interior-details-of-modern-bar-with-wooden-furniture-and-arched-wall-5490909/)
-- [Photo by Max Vakhtbovych from Pexels](https://www.pexels.com/photo/contemporary-apartment-with-cozy-sofa-and-minimalist-furniture-6585764/)
-- [Photo by Dayvison de Oliveira Silva from Pexels](https://www.pexels.com/photo/cozy-living-room-design-with-comfy-sofa-and-creative-lamp-5696287/)
-- [Photo by Kien Tran from Pexels](https://www.pexels.com/photo/hotel-house-table-luxury-5814952/)
-- [Photo by Nugroho Wahyu from Pexels](https://www.pexels.com/photo/brown-wooden-table-with-chair-3097112/)
-
-
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,134 +1,122 @@
 import Link from "next/link";
-import Script from "next/script";
-import "uikit/dist/css/uikit.min.css";
-// import 'uikit/dist/js/uikit.min.js';
-// import 'uikit/dist/js/uikit-icons.min.js';
+import "../styles/globals.css";
 
 export default function ({ children }) {
-  return (
-    <html>
-      <body>
-        {/* header */}
-        <div className="uk-padding-small uk-box-shadow-medium">
-          <div className="uk-flex uk-flex-center uk-flex-middle">
-            <div>
-              <Link
-                href="/"
-                className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
-              >
-                Home
-              </Link>
-            </div>
-            <div>
-              <Link
-                href="/service"
-                className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
-              >
-                Service
-              </Link>
-            </div>
-            <div>
-              <Link
-                href="/#about"
-                className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
-              >
-                About
-              </Link>
-            </div>
-            <div>
-              <Link
-                href="/#contact"
-                className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
-              >
-                Contact
-              </Link>
-            </div>
-          </div>
-        </div>
+	return (
+		<html>
+			<body>
+				{/* header */}
+				<div className="uk-padding-small uk-box-shadow-medium">
+					<div className="uk-flex uk-flex-center uk-flex-middle">
+						<div>
+							<Link
+								href="/"
+								className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
+							>
+								Home
+							</Link>
+						</div>
+						<div>
+							<Link
+								href="/service"
+								className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
+							>
+								Service
+							</Link>
+						</div>
+						<div>
+							<Link
+								href="/#about"
+								className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
+							>
+								About
+							</Link>
+						</div>
+						<div>
+							<Link
+								href="/#contact"
+								className="uk-link-reset uk-text-light uk-padding-small uk-text-large"
+							>
+								Contact
+							</Link>
+						</div>
+					</div>
+				</div>
 
-        {/* body */}
-        {children}
+				{/* body */}
+				{children}
 
-        {/* footer */}
-        <div className="uk-section uk-section-large">
-          <div className="uk-container uk-text-center">
-            <div
-              className="uk-child-width-1-2@s uk-child-width-1-4@l"
-              uk-grid=""
-            >
-              <div className="">
-                <a
-                  className="uk-icon-button uk-margin-small-right"
-                  uk-icon="facebook"
-                  href="#"
-                ></a>
-                <a
-                  className="uk-icon-button uk-margin-small-right"
-                  uk-icon="instagram"
-                  href="#"
-                ></a>
-                <a
-                  className="uk-icon-button uk-margin-small-right"
-                  uk-icon="pinterest"
-                  href="#"
-                ></a>
-                <a
-                  className="uk-icon-button uk-margin-small-right"
-                  uk-icon="twitter"
-                  href="#"
-                ></a>
-                <a
-                  className="uk-icon-button uk-margin-small-right"
-                  uk-icon="youtube"
-                  href="#"
-                ></a>
-              </div>
+				{/* footer */}
+				<div className="uk-section uk-section-large">
+					<div className="uk-container uk-text-center">
+						<div
+							className="uk-child-width-1-2@s uk-child-width-1-4@l"
+							uk-grid=""
+						>
+							<div className="">
+								<a
+									className="uk-icon-button uk-margin-small-right"
+									uk-icon="facebook"
+									href="#"
+								></a>
+								<a
+									className="uk-icon-button uk-margin-small-right"
+									uk-icon="instagram"
+									href="#"
+								></a>
+								<a
+									className="uk-icon-button uk-margin-small-right"
+									uk-icon="pinterest"
+									href="#"
+								></a>
+								<a
+									className="uk-icon-button uk-margin-small-right"
+									uk-icon="twitter"
+									href="#"
+								></a>
+								<a
+									className="uk-icon-button uk-margin-small-right"
+									uk-icon="youtube"
+									href="#"
+								></a>
+							</div>
 
-              <div>
-                <h3>Paragraph</h3>
-                <p>
-                  <span>Made by zummon (Teerapat Anantarattanachai)</span>
-                </p>
-              </div>
+							<div>
+								<h3>Paragraph</h3>
+								<p>
+									<span>Made by zummon (Teerapat Anantarattanachai)</span>
+								</p>
+							</div>
 
-              <div>
-                <h3>List</h3>
-                <ul className="uk-list">
-                  <li>Lorem, ipsum.</li>
-                  <li>Lorem, ipsum.</li>
-                </ul>
-              </div>
+							<div>
+								<h3>List</h3>
+								<ul className="uk-list">
+									<li>Lorem, ipsum.</li>
+									<li>Lorem, ipsum.</li>
+								</ul>
+							</div>
 
-              <div>
-                <h3>List</h3>
-                <ul className="uk-list">
-                  <li>
-                    <a href="#">Lorem, ipsum.</a>
-                  </li>
-                  <li>
-                    <a href="#">Lorem, ipsum.</a>
-                  </li>
-                  <li>
-                    <a href="#">Lorem, ipsum.</a>
-                  </li>
-                  <li>
-                    <a href="#">Lorem, ipsum.</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* maybe need to fix for best production */}
-      </body>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/uikit@3.16.6/dist/js/uikit.min.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/uikit@3.16.6/dist/js/uikit-icons.min.js"
-        strategy="lazyOnload"
-      />
-    </html>
-  );
+							<div>
+								<h3>List</h3>
+								<ul className="uk-list">
+									<li>
+										<a href="#">Lorem, ipsum.</a>
+									</li>
+									<li>
+										<a href="#">Lorem, ipsum.</a>
+									</li>
+									<li>
+										<a href="#">Lorem, ipsum.</a>
+									</li>
+									<li>
+										<a href="#">Lorem, ipsum.</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</body>
+		</html>
+	);
 }
